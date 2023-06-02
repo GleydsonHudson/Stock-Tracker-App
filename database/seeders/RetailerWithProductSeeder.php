3bed2c284718a4d96c15358bb2e5be0dc480f9cb
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-02 16:23:28 +0200

    Create the seeder RetailerWithProductSeeder

---
+++ database/seeders/RetailerWithProductSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\Stock;
+use Illuminate\Database\Seeder;
+
+class RetailerWithProductSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $bestBuy->addStock($switch, new Stock([
+            'price'    => 10000,
+            'url'      => 'http://foo.com',
+            'sku'      => '12345',
+            'in_stock' => false,
+        ]));
+    }
+}
