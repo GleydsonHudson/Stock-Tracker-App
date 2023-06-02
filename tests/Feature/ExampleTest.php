22305d4609fe8151b2f7e0973ac8dc1b5583bf5e
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-02 16:07:21 +0200

    Create a test to test the TrackCommand

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,36 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+ use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\Stock;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic test example.
+     * @test
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function it_checks_stocks_for_products_at_retailers(): void
     {
-        $response = $this->get('/');
+        $switch = Product::create(['name' => 'Nintendo Switch']);
 
-        $response->assertStatus(200);
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => 10000,
+            'url' => 'http://foo.com',
+            'sku' => '12345',
+            'in_stock' => true,
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
+
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,36 +2,9 @@
 
 namespace Tests\Unit;
 
-use App\Models\Product;
-use App\Models\Retailer;
-use App\Models\Stock;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
-    /**
-     * @test
-     */
-    public function it_checks_stocks_for_products_at_retailers(): void
-    {
-        $switch = Product::create(['name' => 'Nintendo Switch']);
 
-        $bestBuy = Retailer::create(['name' => 'Best Buy']);
-
-        $this->assertFalse($switch->inStock());
-
-        $stock = new Stock([
-            'price' => 10000,
-            'url' => 'http://foo.com',
-            'sku' => '12345',
-            'in_stock' => true,
-        ]);
-
-        $bestBuy->addStock($switch, $stock);
-        
-        $this->assertTrue($switch->inStock());
-
-    }
 }
