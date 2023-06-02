6c56a2a61e48c4130995ea9141e6c8b8fa20ded3
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-02 16:07:47 +0200

    Add the necessary logic to track a stock.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,11 @@ class Product extends Model
 {
     use HasFactory;
 
+    public function track(): void
+    {
+        $this->stock->each->track();
+    }
+
     public function inStock(): bool
     {
         return $this->stock()->where('in_stock', true)->exists();
--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -3,9 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Stock extends Model
 {
+
     use HasFactory;
 
     protected $table = 'stock';
+
+    protected $casts = [
+        'in_stock' => 'boolean',
+    ];
+
+    public function track(): void
+    {
+        if ($this->retailer->name === 'Best Buy') {
+            // Hit an API endpoint for the associated retailer
+            // Fetch the up-to-date details for the item
+            $results = \Http::get('http://foo.test')->json();
+
+            // And then refresh the current stock record.
+            $this->update([
+                'in_stock' => $results['available'],
+                'price'    => $results['price'],
+            ]);
+        }
+    }
+
+
+    public function retailer(): BelongsTo
+    {
+        return $this->belongsTo(Retailer::class);
+    }
 }
