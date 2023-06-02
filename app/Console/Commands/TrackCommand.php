f650c286cd184bd81def20dd35c83047f04b6de5
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-02 16:06:54 +0200

    Create the command to track a stock.

---
+++ app/Console/Commands/TrackCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class TrackCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'track';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Track all product stock.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Product::all()->each->track();
+    }
+}
